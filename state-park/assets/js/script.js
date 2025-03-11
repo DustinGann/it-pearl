9d5e592ba8034b0e40a5932ca0a128870adac992
DustinGann <123513919+DustinGann@users.noreply.github.com>
2025-03-11 19:54:14 +0000

    lel

--- state-park/assets/js/script.js
+++ state-park/assets/js/script.js
@@ -33,9 +33,85 @@ async function GetTemp() {
             let myURL2 = "https://api.open-meteo.com/v1/forecast?latitude="+ latitude+"&longitude="+longitude +"&current=temperature_2m&hourly=temperature_2m&temperature_unit=fahrenheit";
             //  https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m&hourly=temperature_2m&temperature_unit=fahrenheit
             //  https://api.open-meteo.com/v1/forecast?latitude=34.23&longitude=-93.73&current=temperature_2m&hourly=temperature_2m&temperature_unit=fahrenheit
-
            
-            alert(myURL2);
+            let msg3Object = await fetch(myURL2);
+
+            if (msg2Object.status >= 200 && msg2Object.status <= 299) { ///////////
+                 let msg3JSONText = await msg3Object.text();
+
+                 let msg3 = JSON.parse(msg3JSONText);
+
+                 //local string
+                 
+            
+
+                let Heatdate = [];
+                let Heatvalue = [];
+
+                
+                 
+                let numdays = msg3.hourly.time.length;
+                if (numdays > 0) {
+                    for (let i = 0; i < numdays; i++) {
+                        /* stock close value */
+                        Heatvalue[i] = msg3.hourly.temperature_2m[i];
+                        /* stock volume */
+                      
+                        /* date is in Unix milleseconds - create a temporary date variable */
+                        let tempdate = new Date(msg3.hourly.time[i]);
+                        /* extract the date string from the value */
+                        Heatdate[i] = tempdate.toLocaleString();
+
+                        //toLocalDateString()
+                        //toLocalTimeString()
+                    }
+                }
+
+                let Heatvaluetable = "";
+                if (numdays > 0) {
+                    Heatvaluetable = Heatvaluetable + "<table><caption>Temprature </caption><tr><th>Date</th><th>Price</th></tr>";
+                    for (let i = 0; i < numdays; i++) {
+                        Heatvaluetable = Heatvaluetable + "<tr><td>" + Heatdate[i] + "</td><td>" + Heatvalue[i] + "</td></tr>";
+                    }
+                    Heatvaluetable = Heatvaluetable + "</table>"
+                    document.getElementById("Heatvaluetable").innerHTML = Heatvaluetable;
+
+                    
+                }
+
+                let ctx1 = document.getElementById("chartjs-1");
+                let myChart1 = new Chart(ctx1, {
+                    "type":"line",
+                    "data": {
+                        "labels": Heatdate,
+                        "datasets":[{"label":"Temperature",
+                        "data": Heatvalue,
+                        "fill":false,
+                        "borderColor":"rgb(255, 214, 48)",
+                        "lineTension":0.1}]},
+                        "options":{ 
+                            responsive: false,
+                            maintainAspectRatio: true,
+                        }
+                    }
+                );
+
+            }
+            else{
+                alert("fail");
+            }
+
+
+
+
+
+
+
+
+
+
+
+            
 
         }
         else{
